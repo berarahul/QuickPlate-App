8072bac49fd5186bf8a0348bbd34a49beb712050
rahul <rb6764385@gmail.com>
2026-03-30 22:25:25 +0530

    Folder structure setup and login and registartion file create for student

---
+++ lib/features/auth/views/student_login.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StudentLogin extends StatelessWidget {
+  const StudentLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: 
+    
+    AppBar(title: Text("Student Login"),
+    ));
+  }
+}
---
+++ lib/features/auth/views/student_registration.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StudentRegistration extends StatelessWidget {
+  const StudentRegistration({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Student Registration"),
+      ),
+    );
+  }
+}
